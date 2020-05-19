40020939c56a018bb751149a34c8810d7376af7a
Tristan Coignion <tristan.coignion@gmail.com>
2020-05-19 17:01:41 +0200

    feat: update Invocation for Truffle
    Added child elements and an executeGeneric function

--- src/main/java/ini/ast/Invocation.java
+++ src/main/java/ini/ast/Invocation.java
@@ -5,18 +5,41 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.oracle.truffle.api.CallTarget;
+import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.ExplodeLoop;
+import com.oracle.truffle.api.nodes.IndirectCallNode;
 
 import ini.parser.IniParser;
 
+/**
+ * Invocations share the same name as the function or process they invoke
+ * 
+ * If the function is not found within the current context,
+ * searchs in the root context
+ * 
+ * 
+ */
 public class Invocation extends NamedElement implements Statement, Expression {
 
 	public List<Expression> arguments;
+	@Child protected AstElement functionNode;
+	@Children protected final AstElement[] argumentNodes;
+	@Child protected IndirectCallNode callNode;
 
+	@Deprecated
 	public Invocation(IniParser parser, Token token, String name, List<Expression> arguments) {
+		this(parser,token,name,null,null);
+	}
+	
+	public Invocation(IniParser parser, Token token, String name, AstElement functionNode, AstElement[] argumentNodes) {
 		super(parser, token, name);
-		this.arguments = arguments;
 		this.nodeTypeId = AstNode.INVOCATION;
+		this.functionNode = functionNode;
+		this.argumentNodes = argumentNodes;
+		this.callNode = Truffle.getRuntime().createIndirectCallNode();
 	}
 
 	@Override
@@ -37,9 +60,27 @@ public class Invocation extends NamedElement implements Statement, Expression {
 	}
 
 	@Override
+	@ExplodeLoop
 	public Object executeGeneric(VirtualFrame virtualFrame) {
-		// TODO Auto-generated method stub
-		return null;
+		Executable function = (Executable) this.functionNode.executeGeneric(virtualFrame);
+		
+        /*
+         * The number of arguments is constant for one invoke node. During compilation, the loop is
+         * unrolled and the execute methods of all arguments are inlined. This is triggered by the
+         * ExplodeLoop annotation on the method. The compiler assertion below illustrates that the
+         * array length is really constant.
+         */
+        CompilerAsserts.compilationConstant(this.argumentNodes.length);
+		
+		Object[] argumentValues = new Object[this.argumentNodes.length + 1];
+		for(int i=0; i<this.argumentNodes.length; i++) {
+			argumentValues[i] = this.argumentNodes[i].executeGeneric(virtualFrame);
+		}
+		return call(virtualFrame, function.callTarget, argumentValues);
 	}
 	
+    protected Object call(VirtualFrame virtualFrame, CallTarget callTarget,
+            Object[] arguments) {
+        return this.callNode.call(callTarget, arguments);
+    }
 }
