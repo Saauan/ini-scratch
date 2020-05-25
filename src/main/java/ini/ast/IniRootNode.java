8e5adc8f07468683554d0c5a7020a9bceb225433
Tristan Coignion <tristan.coignion@gmail.com>
2020-05-25 15:03:10 +0200

    fix: IniRootNode bad sequence iteration

--- src/main/java/ini/ast/IniRootNode.java
+++ src/main/java/ini/ast/IniRootNode.java
@@ -23,9 +23,9 @@ public class IniRootNode extends RootNode {
 	@ExplodeLoop
 	public Object execute(VirtualFrame frame) {
 		Sequence<AstElement> s = this.bodyNodes;
-		int nbNodes = s.size() - 1;
+		int nbNodes = s.size();
 		CompilerAsserts.compilationConstant(nbNodes);
-		for (int i = 0; i < nbNodes - 1; i++) {
+		for (int i = 0; i < nbNodes-1; i++) {
 			s.get().executeGeneric(frame);
 			s = s.next();
 		}
@@ -35,13 +35,16 @@ public class IniRootNode extends RootNode {
 	public static IniRootNode create(IniLanguage lang, FrameSlot[] parametersSlots, Sequence<AstElement> bodyNodes,
 			FrameDescriptor frameDescriptor) {
 		Sequence<AstElement> allNodes = new Sequence<AstElement>(null);
-		for (int arg_index = parametersSlots.length - 1; arg_index >= 0; arg_index--) {
-			allNodes.insertNext(createAssignment(parametersSlots, arg_index));
+		// Insert all parameters
+		if(parametersSlots.length>0) {
+			for (int arg_index = parametersSlots.length - 1; arg_index >= 0; arg_index--) {
+				allNodes.insertNext(createAssignment(parametersSlots, arg_index));
+			}
 		}
 		assert allNodes.get() == null;
-		assert allNodes.size() == parametersSlots.length;
-		allNodes = allNodes.next();
+		assert allNodes.size() == parametersSlots.length + 1;
 		allNodes.last().insertNext(bodyNodes);
+		allNodes = allNodes.next();
 		assert allNodes.size() == parametersSlots.length + bodyNodes.size();
 		return new IniRootNode(lang, allNodes, frameDescriptor);
 	}
@@ -54,6 +57,12 @@ public class IniRootNode extends RootNode {
 		return new Assignment(null, null,
 				new Variable(null, null, argumentNames[arg_index].toString(), argumentNames[arg_index]),
 				new ReadArgumentFromContextNode(null, null, arg_index), argumentNames[arg_index]);
+	}
+
+	@Deprecated
+	public void setName(String string) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
