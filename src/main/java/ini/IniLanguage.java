e7b4229658979707c57ba86b32fde1a661bfcb68
Tristan Coignion <tristan.coignion@gmail.com>
2020-05-28 14:54:48 +0200

    fix: add parse method to IniLanguage
    Not useful now, but may be later

--- src/main/java/ini/IniLanguage.java
+++ src/main/java/ini/IniLanguage.java
@@ -1,27 +1,26 @@
 package ini;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.PrintStream;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 
 import com.google.gson.Gson;
-import com.martiansoftware.jsap.FlaggedOption;
 import com.martiansoftware.jsap.JSAP;
-import com.martiansoftware.jsap.JSAPResult;
-import com.martiansoftware.jsap.Switch;
-import com.martiansoftware.jsap.UnflaggedOption;
+import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.TruffleLanguage;
+import com.oracle.truffle.api.frame.FrameSlot;
+import com.oracle.truffle.api.frame.MaterializedFrame;
 import com.oracle.truffle.api.source.Source;
 
 import ini.ast.AstElement;
 import ini.ast.AstNode;
 import ini.ast.Executable;
+import ini.ast.IniRootNode;
 import ini.parser.IniParser;
+import ini.runtime.IniFunction;
 
 /**
  * The entry point for the INI parser/evaluator.
@@ -29,7 +28,7 @@ import ini.parser.IniParser;
  * @author Renaud Pawlak
  */
 @TruffleLanguage.Registration(name = "INI", id="INI")
-public class IniLanguage extends TruffleLanguage<Object>{
+public class IniLanguage extends TruffleLanguage<IniContext>{
 	
 	public static final Logger LOGGER = LoggerFactory.getLogger("ini");
 
@@ -38,12 +37,49 @@ public class IniLanguage extends TruffleLanguage<Object>{
 	public static final String ID = "INI";
 	public static final String MIME_TYPE = "application/x-ini";
 	
-//	private static void runIni(String filename) throws IOException {
-//	}
+	
 
 	@Override
 	protected IniContext createContext(Env env) {
-		return new IniContext();
+		return new IniContext(this, env);
+	}
+	
+    public static IniContext getCurrentContext() {
+        return getCurrentContext(IniLanguage.class);
+    }
+    
+    @Override
+    protected CallTarget parse(ParsingRequest request) throws Exception{
+    	Source source = request.getSource();
+		IniParser parser = IniParser.createParserForCode(null, null, source.getCharacters().toString());
+		try {
+			parser.parse();
+		} catch (Exception e) {
+			if (parser.hasErrors()) {
+				parser.printErrors(System.err);
+				e.printStackTrace();
+				System.exit(1);
+			} else {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+		AstElement[] topLevelNodes = parser.topLevels.toArray(new AstElement[0]);
+		MaterializedFrame globalFrame = getCurrentContext().getGlobalFrame();
+		IniFunction function = wrapNodesAndCreateCallTarget(topLevelNodes, globalFrame);
+        return function.callTarget;
+    }
+
+    
+	private IniFunction wrapNodesAndCreateCallTarget(AstElement[] topLevelNodes, MaterializedFrame globalFrame) {
+		IniFunction function = IniFunction.create(
+        		null,
+        		new FrameSlot[] {},
+        		topLevelNodes,
+        		globalFrame.getFrameDescriptor());
+        function.setLexicalScope(globalFrame);
+        ((IniRootNode) function.callTarget.getRootNode()).setName("main"); // TODO : Make a real main
+		return function;
 	}
 
 	@Override
