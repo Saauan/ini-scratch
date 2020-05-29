c7e65085400ae93b0d5ac8fb3acfd729d81c0bd6
Tristan Coignion <tristan.coignion@gmail.com>
2020-05-29 11:18:19 +0200

    tests: add tests for println and variable assignement

--- src/test/java/ini/test/eval/TestExamples.java
+++ src/test/java/ini/test/eval/TestExamples.java
@@ -7,9 +7,19 @@ public class TestExamples extends IniTestCase {
 	public TestExamples(String name) {
 		super(name);
 	}
+	
+	public void testPrintln() {
+		testFile("ini/truffle/testPrintln.ini", outputStream, 
+				(p, out) -> assertEquals("hello world"+ System.lineSeparator() + "hello world", out));
+	}
 
 	public void testPrint() {
-		testFile("ini/truffle/testPrint.ini",
+		testFile("ini/truffle/testPrint.ini", outputStream,
 				(p, out) -> assertEquals("hello worldhello world", out));
+	}
+	
+	public void testVariable() {
+		testFile("ini/truffle/testVariable.ini", outputStream, 
+				(p, out) -> assertEquals("5\n5\n", out));
 	}
 }
