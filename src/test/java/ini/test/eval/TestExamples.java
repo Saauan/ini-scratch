f68b0e51f819b7e34a4b85fb89f3185efc869e65
Tristan Coignion <tristan.coignion@gmail.com>
2020-05-29 11:23:33 +0200

    test: changed how newline behave in strings

--- src/test/java/ini/test/eval/TestExamples.java
+++ src/test/java/ini/test/eval/TestExamples.java
@@ -9,17 +9,19 @@ public class TestExamples extends IniTestCase {
 	}
 	
 	public void testPrintln() {
-		testFile("ini/truffle/testPrintln.ini", outputStream, 
+		testFile("ini/truffle/testPrintln.ini", 
 				(p, out) -> assertEquals("hello world"+ System.lineSeparator() + "hello world", out));
 	}
 
 	public void testPrint() {
-		testFile("ini/truffle/testPrint.ini", outputStream,
+		testFile("ini/truffle/testPrint.ini",
 				(p, out) -> assertEquals("hello worldhello world", out));
 	}
 	
 	public void testVariable() {
-		testFile("ini/truffle/testVariable.ini", outputStream, 
-				(p, out) -> assertEquals("5\n5\n", out));
+		testFile("ini/truffle/testVariable.ini", 
+				(p, out) -> assertEquals("5"
+						+ System.lineSeparator() + "5"
+						+ System.lineSeparator(), out));
 	}
 }
