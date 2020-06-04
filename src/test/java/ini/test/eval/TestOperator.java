6449abe27df0d4406507e5fec89ad4b3ab6fda90
Tristan Coignion <tristan.coignion@gmail.com>
2020-06-04 18:58:40 +0200

    build: change test folder name

--- src/test/java/ini/test/eval/TestOperator.java
+++ src/test/java/ini/test/eval/TestOperator.java
@@ -2,34 +2,34 @@ package ini.test.eval;
 
 import ini.test.IniTestCase;
 
-public class TestBinary extends IniTestCase {
+public class TestOperator extends IniTestCase {
 
-	public TestBinary(String name) {
+	public TestOperator(String name) {
 		super(name);
 	}
-	
+
 	public void testAdd() {
-		testFile("ini/truffle/binary/TestAdd.ini", 
+		testFile("ini/truffle/operator/TestAdd.ini",
 				(p, out) -> assertEquals("n3 = n1 + n2 = 8" + nl + "n4 = 5+3 = 8" + nl, out));
 	}
-	
+
 	public void testMul() {
-		testFile("ini/truffle/binary/TestMul.ini", 
+		testFile("ini/truffle/operator/TestMul.ini",
 				(p, out) -> assertEquals("n3 = n1 * n2 = 15" + nl + "n4 = 5*3 = 15" + nl, out));
 	}
-	
+
 	public void testDiv() {
-		testFile("ini/truffle/binary/TestDiv.ini", 
+		testFile("ini/truffle/operator/TestDiv.ini",
 				(p, out) -> assertEquals("n3 = n1 / n2 = 5" + nl + "n4 = 10/2 = 5" + nl, out));
 	}
-	
+
 	public void testSub() {
-		testFile("ini/truffle/binary/TestSub.ini", 
+		testFile("ini/truffle/operator/TestSub.ini",
 				(p, out) -> assertEquals("n3 = n1 - n2 = 2" + nl + "n4 = 5-3 = 2" + nl, out));
 	}
 
 	public void testEquals() {
-		testFile("ini/truffle/binary/TestEquality.ini",
+		testFile("ini/truffle/operator/TestEquality.ini",
 				(p, out) -> assertEquals("5==5 : true" + nl + 
 						"true==true : true" + nl + 
 						"\"hey\"==\"hey\" : true" + nl + 
