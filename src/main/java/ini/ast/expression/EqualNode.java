9c025499481133e422280c9b2618dd676f13a54e
Tristan Coignion <tristan.coignion@gmail.com>
2020-06-04 18:44:46 +0200

    feat: add EqualNode and tests

---
+++ src/main/java/ini/ast/expression/EqualNode.java
@@ -0,0 +1,85 @@
+package ini.ast.expression;
+
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.Specialization;
+
+import ini.ast.Token;
+import ini.parser.IniParser;
+import ini.runtime.IniException;
+
+public abstract class EqualNode extends BinaryNode {
+
+	public EqualNode(IniParser parser, Token token) {
+		super(parser, token);
+		// TODO Auto-generated constructor stub
+	}
+	
+    @Specialization
+    protected boolean equal(boolean left, boolean right) {
+    	return left == right;
+    }
+    
+    @Specialization
+    protected boolean equal(char left, char right) {
+    	return left == right;
+    }
+    
+    @Specialization
+    protected boolean equal(byte left, byte right) {
+    	return left == right;
+    }
+    
+    @Specialization
+    protected boolean equal(int left, int right) {
+    	return left == right;
+    }
+    
+    @Specialization
+    protected boolean equal(long left, long right) {
+    	return left == right;
+    }
+    
+    @Specialization
+    protected boolean equal(float left, float right) {
+    	return left == right;
+    }
+    
+    @Specialization
+    protected boolean equal(double left, double right) {
+    	return left == right;
+    }
+    
+    @Specialization
+    protected boolean equal(String left, String right) {
+    	return left.equals(right);
+    }
+
+    /**
+     * We covered all the cases that can return true in the type specializations above. If we
+     * compare two values with different types, the result is known to be false.
+     * <p>
+     * Note that the guard is essential for correctness: without the guard, the specialization would
+     * also match when the left and right value have the same type. The following scenario would
+     * return a wrong value: First, the node is executed with the left value 42 (type Number) and the
+     * right value "abc" (String). This specialization matches, and since it is the first execution
+     * it is also the only specialization. Then, the node is executed with the left value "42" (type
+     * Number) and the right value "42" (type Number). Since this specialization is already present, and
+     * (without the guard) also matches (Number are children of Object), it is executed. The
+     * wrong return value is "false".
+     */
+    @Specialization(guards = "differentClasses(left, right)")
+    protected boolean equal(Object left, Object right) {
+        assert !left.equals(right);
+        return false;
+    }
+
+    static boolean differentClasses(Object left, Object right) {
+        return left.getClass() != right.getClass();
+    }
+
+    @Fallback
+    protected Object typeError(Object left, Object right) {
+    	throw IniException.typeError(this, left, right);
+    }
+
+}
--- src/test/java/ini/test/eval/TestBinary.java
+++ src/test/java/ini/test/eval/TestBinary.java
@@ -10,25 +10,36 @@ public class TestBinary extends IniTestCase {
 	
 	public void testAdd() {
 		testFile("ini/truffle/binary/TestAdd.ini", 
-				(p, out) -> assertEquals("n3 = n1 + n2 = 8" + nl +
-						"n4 = 5+3 = 8" + nl, out));
+				(p, out) -> assertEquals("n3 = n1 + n2 = 8" + nl + "n4 = 5+3 = 8" + nl, out));
 	}
 	
 	public void testMul() {
 		testFile("ini/truffle/binary/TestMul.ini", 
-				(p, out) -> assertEquals("n3 = n1 * n2 = 15" + nl +
-						"n4 = 5*3 = 15" + nl, out));
+				(p, out) -> assertEquals("n3 = n1 * n2 = 15" + nl + "n4 = 5*3 = 15" + nl, out));
 	}
 	
 	public void testDiv() {
 		testFile("ini/truffle/binary/TestDiv.ini", 
-				(p, out) -> assertEquals("n3 = n1 / n2 = 5" + nl +
-						"n4 = 10/2 = 5" + nl, out));
+				(p, out) -> assertEquals("n3 = n1 / n2 = 5" + nl + "n4 = 10/2 = 5" + nl, out));
 	}
 	
 	public void testSub() {
 		testFile("ini/truffle/binary/TestSub.ini", 
-				(p, out) -> assertEquals("n3 = n1 - n2 = 2" + nl +
-						"n4 = 5-3 = 2" + nl, out));
+				(p, out) -> assertEquals("n3 = n1 - n2 = 2" + nl + "n4 = 5-3 = 2" + nl, out));
+	}
+
+	public void testEquals() {
+		testFile("ini/truffle/binary/TestEquality.ini",
+				(p, out) -> assertEquals("5==5 : true" + nl + 
+						"true==true : true" + nl + 
+						"\"hey\"==\"hey\" : true" + nl + 
+						"3.14==3.14 : true" + nl + 
+						"5==4 : false" + nl + 
+						"true==false : false" + nl + 
+						"\"hey\"==\"ho\" : false" + nl + 
+						"3.14==3.42 : false" + nl + 
+						"\"3.14\"==3.14 : false" + nl, out));
+	}
+	
 	}
 }
